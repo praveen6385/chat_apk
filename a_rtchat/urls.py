1c20c45ceb0eee9d7b430b39c4bf90b8af0041af
praveen6385 <romapraveen25@gmail.com>
2024-12-08 00:54:07 -0800

    Intial commit

---
+++ a_rtchat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', chat_view, name='home'),
+    path('chat/<username>', get_or_create_chatroom, name="start-chat"),
+    path('chat/room/<chatroom_name>', chat_view, name="chatroom"),
+    path('profile/<username>', profile_view, name='profile'),
+]
